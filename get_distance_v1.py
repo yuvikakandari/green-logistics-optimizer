9a967590f01d405907d3610ce033f0d27e0ed6f8
Yuvika Kandari <yuvika7kandari@gmail.com>
2025-06-25 17:27:11 +0530

    Add working route script as get_distance_v1.py

---
+++ get_distance_v1.py
@@ -0,0 +1,6 @@
+import openrouteservice
+client = openrouteservice.Client(key="5b3ce3597851110001cf6248729ae0da3c6342289d21452129e44d18")
+coordinates = ((77.2090, 28.6139), (75.7873, 26.9124))
+route = client.directions(coordinates=coordinates, profile='driving-car')
+distance_km = route['routes'][0]['summary']['distance'] / 1000
+print(f"Distance from Delhi to Jaipur: {distance_km:.2f} km")
