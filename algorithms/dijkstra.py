da6dfc26c16e6352e58e5ce559a017aa1cc0e182
Yuvika Kandari <yuvika7kandari@gmail.com>
2026-04-17 18:53:00 +0530

    replaces previous file

--- algorithms/dijkstra.py
+++ algorithms/dijkstra.py
@@ -1,20 +1,10 @@
 import networkx as nx
 
-
-def build_graph():
-    graph = nx.Graph()
-
-    graph.add_edge("A", "B", weight=4)
-    graph.add_edge("A", "D", weight=2)
-    graph.add_edge("B", "C", weight=3)
-    graph.add_edge("B", "E", weight=2)
-    graph.add_edge("D", "E", weight=3)
-    graph.add_edge("E", "C", weight=2)
-
-    return graph
+from algorithms.graph_builder import build_graph
 
 
 def shortest_path(source, destination):
+
     graph = build_graph()
 
     path = nx.dijkstra_path(
@@ -32,10 +22,3 @@ def shortest_path(source, destination):
     )
 
     return path, distance
-
-
-if __name__ == "__main__":
-    path, distance = shortest_path("A", "C")
-
-    print("Path:", path)
-    print("Distance:", distance)
