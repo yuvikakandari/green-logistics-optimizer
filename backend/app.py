3a307557fade48073e67c91002378fdf0f63f627
Yuvika Kandari <yuvika7kandari@gmail.com>
2026-04-20 18:53:00 +0530

    real route fastapi endpoint

--- backend/app.py
+++ backend/app.py
@@ -24,3 +24,18 @@ def route(
         source,
         destination
     )
+
+@app.get("/real-route")
+def real_route(
+    source_lat: float,
+    source_lon: float,
+    dest_lat: float,
+    dest_lon: float
+):
+
+    return get_route(
+        source_lat,
+        source_lon,
+        dest_lat,
+        dest_lon
+    )
